1e7783fde9c10f550d1c13ee48fe6ee3f1979aec
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 09:34:34 +0200

    Add camera movement script

---
+++ defender-clone/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private Transform player;
+    private Vector2 targetPosition;
+
+    void Awake()
+    {
+        player = Component.FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    void OnEnable()
+    {
+        EventManager.OnButtonHold += SetTargetPosition;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, 0, -10);
+    }
+
+    private void SetTargetPosition(Vector2 position, int touchId)
+    {
+        targetPosition = Vector2.Lerp(player.position, Camera.main.ScreenToWorldPoint(position), 0.3f);
+    }
+}
