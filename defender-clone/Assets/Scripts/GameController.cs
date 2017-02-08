186d844094f67c8b9c08775b9d582ef21bafdfa3
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 10:29:44 +0200

    GameController to track if player exist

--- defender-clone/Assets/Scripts/GameController.cs
+++ defender-clone/Assets/Scripts/GameController.cs
@@ -6,9 +6,23 @@ public class GameController : MonoBehaviour
 {
     public Camera cam;
     public float levelWidth = 70;
+    public GameObject player;
 
     void Awake()
     {
         cam = Camera.main;
+    }
+
+    void OnEnable()
+    {
+        player = Component.FindObjectOfType<PlayerController>().gameObject;
+    }
+
+    void Update()
+    {
+        if (player == null)
+        {
+            print("Lost a life!");
+        }
     }
 }
