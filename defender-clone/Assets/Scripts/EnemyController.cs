4bcca9c7c924a6d7011bfcedb1005293cd2bad04
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 10:18:07 +0200

    Add enemy controller, destroyable enemies

---
+++ defender-clone/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Player Bullet"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
