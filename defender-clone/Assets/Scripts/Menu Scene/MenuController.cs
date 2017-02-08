ba1d4221fcff85436535ee15f56b1ac8737316fc
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 11:26:56 +0200

    Add menu scene and logic

---
+++ defender-clone/Assets/Scripts/Menu Scene/MenuController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private AsyncOperation sceneLoadOperation;
+    private bool menuFinished = false;
+
+    void Start()
+    {
+        StartCoroutine(EnableChildren());
+        sceneLoadOperation = SceneManager.LoadSceneAsync("Game");
+        sceneLoadOperation.allowSceneActivation = false;
+    }
+
+    void Update()
+    {
+        if (menuFinished && Input.GetMouseButtonUp(0))
+        {
+            sceneLoadOperation.allowSceneActivation = true;
+            menuFinished = false;
+        }
+    }
+
+    private IEnumerator EnableChildren()
+    {
+        yield return new WaitForSeconds(0.2f);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(true);
+            if (i < transform.childCount - 2)
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+            else
+            {
+                yield return new WaitForSeconds(1.5f);
+                menuFinished = true;
+            }
+        }
+    }
+}
---
+++ defender-clone/Assets/Scripts/Menu Scene/MenuLineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLineController : MonoBehaviour
+{
+    private Vector2 targetPosition;
+    public bool appearFromRight = true;
+
+    void Awake()
+    {
+        targetPosition = transform.position;
+        float targetX = appearFromRight ? 16 : -16;
+        transform.Translate(new Vector2(targetX, 0));
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, targetPosition, 10f * Time.deltaTime);
+    }
+}
