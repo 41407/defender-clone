37f497f326e84d8def05a331312647f1b4659d6b
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 11:56:05 +0200

    Enemies move towards player

---
+++ defender-clone/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform player;
+    public float speed = 3;
+
+    void Update()
+    {
+        if (player == null)
+        {
+            FindPlayer();
+        }
+        else
+        {
+            MoveTowardsPlayer();
+        }
+    }
+
+    private void FindPlayer()
+    {
+        PlayerController playerController = Component.FindObjectOfType<PlayerController>();
+        if (playerController != null)
+        {
+            player = playerController.transform;
+        }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        transform.Translate((player.position - transform.position).normalized * speed * Time.deltaTime);
+    }
+}
