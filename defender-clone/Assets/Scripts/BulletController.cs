9368505f91d4c630c99d61ec703ada451be7d9c8
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 11:59:29 +0200

    Add Bullet time to live

---
+++ defender-clone/Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float timeToLive = 2;
+
+    void OnEnable()
+    {
+        StartCoroutine(DisableAfterTime(timeToLive));
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        StopCoroutine(DisableAfterTime(timeToLive));
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator DisableAfterTime(float ttl)
+    {
+        yield return new WaitForSeconds(ttl);
+        gameObject.SetActive(false);
+    }
+}
