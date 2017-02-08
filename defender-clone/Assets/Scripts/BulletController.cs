2add4d4e66d64441120d28a94a368a6b6fa18922
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 15:02:57 +0200

    Prettier enemy bullets

--- defender-clone/Assets/Scripts/BulletController.cs
+++ defender-clone/Assets/Scripts/BulletController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
+    public GameObject explosionParticlePrefab;
     public float timeToLive = 2;
 
     void OnEnable()
@@ -14,12 +15,21 @@ public class BulletController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D coll)
     {
         StopCoroutine(DisableAfterTime(timeToLive));
-        gameObject.SetActive(false);
+        Disable();
     }
 
     private IEnumerator DisableAfterTime(float ttl)
     {
         yield return new WaitForSeconds(ttl);
+        Disable();
+    }
+
+    private void Disable()
+    {
+        if (explosionParticlePrefab)
+        {
+            Factory.create.ByReference(explosionParticlePrefab, transform.position, Quaternion.identity);
+        }
         gameObject.SetActive(false);
     }
 }
