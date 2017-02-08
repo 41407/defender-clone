6b4b6834c8af8fc09a45e4ffc23d1c3fe226445e
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 09:21:45 +0200

    Add event manager for input events

---
+++ defender-clone/Assets/Scripts/EventManager.cs
@@ -0,0 +1,74 @@
+﻿∫using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public delegate void InputAction(Vector2 position, int touchId);
+    public static event InputAction OnButtonDown;
+    public static event InputAction OnButtonUp;
+    public static event InputAction OnButtonHold;
+    private const int NUMBER_OF_MOUSE_BUTTONS = 5;
+
+    void Update()
+    {
+        SendMouseEvents();
+        SendTouchEvents();
+    }
+
+    private void SendMouseEvents()
+    {
+        for (int button = 0; button < NUMBER_OF_MOUSE_BUTTONS; button++)
+        {
+            if (Input.GetMouseButtonDown(button))
+            {
+                if (OnButtonDown != null)
+                {
+                    OnButtonDown(Input.mousePosition, button);
+                }
+            }
+            if (Input.GetMouseButton(button))
+            {
+                if (OnButtonHold != null)
+                {
+                    OnButtonHold(Input.mousePosition, button);
+                }
+            }
+            if (Input.GetMouseButtonUp(button))
+            {
+                if (OnButtonUp != null)
+                {
+                    OnButtonUp(Input.mousePosition, button);
+                }
+            }
+        }
+    }
+
+    private void SendTouchEvents()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (OnButtonDown != null)
+                {
+                    OnButtonDown(touch.position, touch.fingerId);
+                }
+            }
+            if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            {
+                if (OnButtonHold != null)
+                {
+                    OnButtonHold(touch.position, touch.fingerId);
+                }
+            }
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                if (OnButtonUp != null)
+                {
+                    OnButtonUp(touch.position, touch.fingerId);
+                }
+            }
+        }
+    }
+}
