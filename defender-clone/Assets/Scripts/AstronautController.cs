62ccb41633039e4a00120dad20b6b344798ea73d
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 15:59:50 +0200

    Abductable astronauts

---
+++ defender-clone/Assets/Scripts/AstronautController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstronautController : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public GameObject abductor;
+    public bool beingAbducted = false;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnEnable()
+    {
+        beingAbducted = false;
+        abductor = null;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            abductor = coll.gameObject;
+            StartCoroutine(GetAbductedCo());
+        }
+    }
+
+    private IEnumerator GetAbductedCo()
+    {
+        body.isKinematic = true;
+        while (abductor.activeInHierarchy)
+        {
+            transform.position = Vector3.Lerp(transform.position, abductor.transform.position, 0.1f);
+            yield return null;
+        }
+        abductor = null;
+        beingAbducted = false;
+        body.isKinematic = false;
+        body.velocity = Vector2.zero;
+    }
+}
--- defender-clone/Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ defender-clone/Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class EnemyBehaviour : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class EnemyBehaviour : MonoBehaviour
         follower,
         shooter
     }
+    public GameObject astronaut;
+    private bool hasAttemptedAbduction = false;
     public EnemyType enemyType;
     private Transform player;
     public float speed = 3;
@@ -19,13 +22,19 @@ public class EnemyBehaviour : MonoBehaviour
         gameController = Component.FindObjectOfType<GameController>();
     }
 
+    void OnEnable()
+    {
+        astronaut = null;
+        hasAttemptedAbduction = false;
+    }
+
     void Update()
     {
         if (player == null)
         {
             FindPlayer();
         }
-        else
+        else if (astronaut == null)
         {
             switch (enemyType)
             {
@@ -40,6 +49,10 @@ public class EnemyBehaviour : MonoBehaviour
                     break;
             }
         }
+        else
+        {
+            Abduct();
+        }
     }
 
     private void FindPlayer()
@@ -53,7 +66,36 @@ public class EnemyBehaviour : MonoBehaviour
 
     private void MoveTowardsPlayer()
     {
-        transform.Translate((player.position - transform.position).normalized * speed * Time.deltaTime);
+        if (Vector2.Distance(player.position, transform.position) > 20 && !hasAttemptedAbduction)
+        {
+            hasAttemptedAbduction = true;
+            astronaut = FindClosestAstronaut(transform.position);
+            AstronautController ac = astronaut.GetComponent<AstronautController>();
+            if (ac.beingAbducted)
+            {
+                astronaut = null;
+            }
+            else
+            {
+                ac.beingAbducted = true;
+            }
+        }
+        else
+        {
+            transform.Translate((player.position - transform.position).normalized * speed * Time.deltaTime);
+        }
+    }
+
+    private void Abduct()
+    {
+        if (astronaut.GetComponent<AstronautController>().abductor == transform)
+        {
+            transform.Translate(Vector2.up * 1 * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate((astronaut.transform.position - transform.position).normalized * speed * Time.deltaTime);
+        }
     }
 
     private void LurkAroundPlayer()
@@ -67,5 +109,11 @@ public class EnemyBehaviour : MonoBehaviour
             transform.Translate(new Vector2(transform.position.x - player.position.x, 0).normalized * speed * Time.deltaTime);
             GetComponent<EnemyFiring>().FireBurst(player.position - transform.position, Mathf.Clamp(gameController.difficulty / 10, 3, 10));
         }
+    }
+
+    private GameObject FindClosestAstronaut(Vector2 position)
+    {
+        GameObject[] astronauts = GameObject.FindGameObjectsWithTag("Astronaut");
+        return astronauts.First(closest => Vector2.Distance(position, closest.transform.position) == (astronauts.Min(astronaut => Vector2.Distance(position, astronaut.transform.position))));
     }
 }
