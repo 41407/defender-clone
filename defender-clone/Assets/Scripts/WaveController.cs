9c3fefde2232ca4356756054def4092772be9c57
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 16:07:50 +0200

    Add astronaut spawning

--- defender-clone/Assets/Scripts/WaveController.cs
+++ defender-clone/Assets/Scripts/WaveController.cs
@@ -8,6 +8,7 @@ public class WaveController : MonoBehaviour
     public bool waveInProgress = false;
     public GameObject enemySpawnerPrefab;
     public GameObject[] enemyPrefabs;
+    public GameObject astronautPrefab;
 
     void Awake()
     {
@@ -27,16 +28,28 @@ public class WaveController : MonoBehaviour
 
     private IEnumerator StartWave(int difficulty)
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(1);
         float levelWidth = gameController.levelWidth;
-        float step = levelWidth / (Mathf.Max(1, (float)difficulty));
-        Vector2 newEnemySpawnerPosition = Vector2.zero;
+        float step = levelWidth / 5f;
+        Vector2 newSpawnerPosition = Vector2.zero;
+        if (difficulty % 5 == 0 && GameObject.FindGameObjectsWithTag("Astronaut").Length < 4)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Factory.create.ByReference(astronautPrefab, newSpawnerPosition, Quaternion.identity);
+                newSpawnerPosition += new Vector2(i * step, 0);
+                yield return null;
+            }
+        }
+        yield return new WaitForSeconds(1);
+        step = levelWidth / (Mathf.Max(1, (float)difficulty));
         for (int i = 0; i < difficulty; i++)
         {
-            Factory.create.ByReference(enemySpawnerPrefab, newEnemySpawnerPosition + Vector2.up * Random.Range(0, 4), Quaternion.identity, transform);
-            newEnemySpawnerPosition += new Vector2(Random.Range(-step * 0.5f, 2 * step), 0);
+            Factory.create.ByReference(enemySpawnerPrefab, newSpawnerPosition + Vector2.up * Random.Range(0, 4), Quaternion.identity, transform);
+            newSpawnerPosition += new Vector2(Random.Range(-step * 0.5f, 2 * step), 0);
             yield return new WaitForSeconds(Random.Range(0.1f, 1.1f));
         }
+
         while (GetNumberOfAliveEnemies() > 2)
         {
             yield return new WaitForSeconds(0.5f);
