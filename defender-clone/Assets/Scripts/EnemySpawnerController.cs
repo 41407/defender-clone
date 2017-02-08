cf32686e5c9d0c7e7f26dd26face8a2dd61c996e
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 10:27:22 +0200

    Add enemy spawner prefab, update game scene

---
+++ defender-clone/Assets/Scripts/EnemySpawnerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerController : MonoBehaviour
+{
+    public GameObject spawnParticlePrefab;
+    public GameObject enemyPrefab;
+    public float spawnDelay;
+
+    void OnEnable()
+    {
+        StartCoroutine(SpawnAfterDelay(spawnDelay));
+    }
+
+    private IEnumerator SpawnAfterDelay(float delay)
+    {
+        Instantiate(spawnParticlePrefab, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+    }
+}
