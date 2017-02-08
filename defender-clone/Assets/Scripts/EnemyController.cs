fe2246f90804c0a460cbdde9353c88df67e16a42
41407 <jimi.welling@cs.helsinki.fi>
2017-02-08 13:18:42 +0200

    Explosion particles

--- defender-clone/Assets/Scripts/EnemyController.cs
+++ defender-clone/Assets/Scripts/EnemyController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject explosionParticlePrefab;
     public int scoreOnDestroy = 50;
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.layer == LayerMask.NameToLayer("Player Bullet") || coll.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            Factory.create.ByReference(explosionParticlePrefab, transform.position, Quaternion.identity);
             gameObject.SetActive(false);
             Score.score += scoreOnDestroy;
         }
