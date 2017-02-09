37a9f7bde2bfbe9223fbe5020738955d36e1a3f8
41407 <jimi.welling@cs.helsinki.fi>
2017-02-09 13:22:58 +0200

    Add lives display

---
+++ defender-clone/Assets/Scripts/LivesDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesDisplay : MonoBehaviour
+{
+
+    private TextMesh text;
+    private GameController gameController;
+    private string staticText;
+    private int lives = 0;
+
+    void Awake()
+    {
+        gameController = Component.FindObjectOfType<GameController>();
+        text = GetComponent<TextMesh>();
+        staticText = text.text;
+    }
+
+    void Update()
+    {
+        if (lives != gameController.lives)
+        {
+            lives = Mathf.Max(0, gameController.lives);
+            text.text = staticText + lives;
+        }
+    }
+}
