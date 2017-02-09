70fb7b0da3a1881004de4aa06949be84d38ef103
41407 <jimi.welling@cs.helsinki.fi>
2017-02-09 12:03:07 +0200

    Enemy health

--- defender-clone/Assets/Scripts/Enemy/EnemyController.cs
+++ defender-clone/Assets/Scripts/Enemy/EnemyController.cs
@@ -6,20 +6,36 @@ public class EnemyController : MonoBehaviour
 {
     public GameObject[] explosionPrefabs;
     public int scoreOnDestroy = 50;
+    public int maxHealth = 1;
+    private int health = 1;
+
+    void OnEnable()
+    {
+        health = maxHealth;
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.layer == LayerMask.NameToLayer("Player Bullet") || coll.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            if (explosionPrefabs.Length > 0)
+            health--;
+            if (health <= 0)
             {
-                for (int i = 0; i < explosionPrefabs.Length; i++)
-                {
-                    Factory.create.ByReference(explosionPrefabs[i], transform.position, Quaternion.identity);
-                }
+                Explode();
             }
-            gameObject.SetActive(false);
-            Score.score += scoreOnDestroy;
         }
+    }
+
+    private void Explode()
+    {
+        if (explosionPrefabs.Length > 0)
+        {
+            for (int i = 0; i < explosionPrefabs.Length; i++)
+            {
+                Factory.create.ByReference(explosionPrefabs[i], transform.position, Quaternion.identity);
+            }
+        }
+        gameObject.SetActive(false);
+        Score.score += scoreOnDestroy;
     }
 }
