fb112f08fd04a4494c741cb046dec6c71e5955d8
41407 <jimi.welling@cs.helsinki.fi>
2017-02-16 18:55:18 +0200

    EnemySpawnController to use fixedUpdate instead of Co

--- defender-clone/Assets/Scripts/Enemy/EnemySpawnerController.cs
+++ defender-clone/Assets/Scripts/Enemy/EnemySpawnerController.cs
@@ -7,6 +7,7 @@ public class EnemySpawnerController : MonoBehaviour
     private WaveController waveController;
     public GameObject spawnParticlePrefab;
     public float spawnDelay;
+    private float currentTime;
 
     void Awake()
     {
@@ -15,14 +16,17 @@ public class EnemySpawnerController : MonoBehaviour
 
     void OnEnable()
     {
-        StartCoroutine(SpawnAfterDelay(spawnDelay));
+        currentTime = 0;
+        Factory.create.ByReference(spawnParticlePrefab, transform.position, Quaternion.identity);
     }
 
-    private IEnumerator SpawnAfterDelay(float delay)
+    void FixedUpdate()
     {
-        Factory.create.ByReference(spawnParticlePrefab, transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(spawnDelay);
-        Factory.create.ByReference(waveController.GetEnemyPrefab(), transform.position, Quaternion.identity, transform.parent);
-        gameObject.SetActive(false);
+        currentTime += Time.fixedDeltaTime;
+        if (currentTime >= spawnDelay)
+        {
+            Factory.create.ByReference(waveController.GetEnemyPrefab(), transform.position, Quaternion.identity, transform.parent);
+            gameObject.SetActive(false);
+        }
     }
 }
