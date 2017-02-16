231205969f567223d7700a6eb7784e2993db1719
41407 <jimi.welling@cs.helsinki.fi>
2017-02-16 13:08:41 +0200

    Simplify wrap controller

--- defender-clone/Assets/Scripts/WrapPositionController.cs
+++ defender-clone/Assets/Scripts/WrapPositionController.cs
@@ -6,37 +6,19 @@ public class WrapPositionController : MonoBehaviour
 {
     private GameController gameController;
     private float levelWidth;
-    public float checkInterval = 1;
 
     void Awake()
     {
         gameController = Component.FindObjectOfType<GameController>();
-    }
-
-    void OnEnable()
-    {
         levelWidth = gameController.levelWidth;
-        StartCoroutine(TranslateIfOutsideLevelWidth());
     }
 
-    private IEnumerator TranslateIfOutsideLevelWidth()
+    void Update()
     {
-        while (true)
+        float positionDelta = gameController.cam.transform.position.x - transform.position.x;
+        if (Mathf.Abs(positionDelta) > levelWidth / 2)
         {
-            float positionDelta = gameController.cam.transform.position.x - transform.position.x;
-            float sign = Mathf.Sign(positionDelta);
-            if (Mathf.Abs(positionDelta) > levelWidth / 2)
-            {
-                transform.Translate(new Vector2(levelWidth * sign, 0));
-            }
-            if (checkInterval > 0)
-            {
-                yield return new WaitForSeconds(checkInterval);
-            }
-            else
-            {
-                yield return null;
-            }
+            transform.Translate(new Vector2(levelWidth * Mathf.Sign(positionDelta), 0));
         }
     }
 }
