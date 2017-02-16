353be4ad0f05396acf47bd20c2506eef9b4badbf
41407 <jimi.welling@cs.helsinki.fi>
2017-02-16 09:45:24 +0200

    Camera to simply follow player

--- defender-clone/Assets/Scripts/CameraMovement.cs
+++ defender-clone/Assets/Scripts/CameraMovement.cs
@@ -4,23 +4,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private Camera cam;
     private Transform player;
-    private Vector2 targetPosition;
-    private bool holding = false;
-    private int mainTouchId;
-
-    void Awake()
-    {
-        cam = GetComponent<Camera>();
-    }
-
-    void OnEnable()
-    {
-        EventManager.OnButtonDown += AssignMainTouch;
-        EventManager.OnButtonHold += SetTargetPosition;
-        EventManager.OnButtonUp += ReleaseTouch;
-    }
 
     void Update()
     {
@@ -37,41 +21,8 @@ public class CameraMovement : MonoBehaviour
         }
         else
         {
-            transform.position = Vector3.Lerp(transform.position, targetPosition, 10 * Time.deltaTime);
+            transform.position = player.transform.position;
             transform.position = new Vector3(transform.position.x, 0, -10);
-        }
-    }
-
-    private void AssignMainTouch(Vector2 position, int touchId)
-    {
-        if (!holding)
-        {
-            holding = true;
-            mainTouchId = touchId;
-        }
-    }
-
-    private void ReleaseTouch(Vector2 position, int touchId)
-    {
-        if (touchId == mainTouchId)
-        {
-            holding = false;
-        }
-    }
-
-    private void SetTargetPosition(Vector2 position, int touchId)
-    {
-        if (player != null)
-        {
-            if (touchId == mainTouchId)
-            {
-                targetPosition = Vector2.Lerp(player.position, cam.ScreenToWorldPoint(position), 0.3f);
-            }
-            else if (!holding)
-            {
-                holding = true;
-                mainTouchId = touchId;
-            }
         }
     }
 }
